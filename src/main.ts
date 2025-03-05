a9126427d03917a0147ee42bff3989a66b2c2c4d
Kadir Can <kdrcng@gmail.com>
2025-03-05 23:29:25 +0300

    minor

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,12 @@ import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter, Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { provideNzIcons } from 'ng-zorro-antd/icon';
 import { AppComponent } from './app/app.component';
-import { BoardingsComponent } from './app/pages/boardings/boardings.component';
-import { icons } from './app/icons-provider';
-import { DashboardComponent } from './app/pages/dashboard/dashboard.component';
 import { routes } from './app/app.routes';
+import { icons } from './app/icons-provider';
 
 registerLocaleData(en);
 
